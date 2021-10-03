e0b7b8137eed0877dd5218d975eb64f5ffef6028
suwan <jsuwan961205@gmail.com>
2021-10-03 15:54:21 +0900

    [Add] 2021.10.03

---
+++ hash/전화번호목록.py
@@ -0,0 +1,18 @@
+def solution(progresses, speeds):
+    # 7, 3, 9
+    prior_task_day = 0
+    cnt = 0
+    result = []
+    for p, s in zip(progresses, speeds):
+
+        left_percent = 100 - p
+        day_to_finish = left_percent // s + (left_percent % s != 0)
+
+        if prior_task_day < day_to_finish:
+            if cnt != 0:
+                result.append(cnt)
+                cnt = 0
+            prior_task_day = day_to_finish
+        cnt += 1
+    result.append(cnt)
+    return result
